d442b0fc167fc9fc0dbe0d7298a363609ea310f1
Burhanuddin Ahmad <burhanahmeed@gmail.com>
2016-11-28 07:53:34 +0700

    Update and rename Login.php to Auth.php

--- Controller/Auth.php
+++ Controller/Auth.php
@@ -6,11 +6,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * 
  * @extends CI_Controller
  */
-class Login extends CI_Controller {
+class Auth extends CI_Controller {
 
 	
 	public function index() {
-
+		redurect('Auth/login');		
+		
+	}
+	
+	function login(){
 		if ($this->session->userdata('username')) 
 		{	
 			redirect('dasbor');
@@ -23,14 +27,12 @@ class Login extends CI_Controller {
 		$valid->set_rules("username","Username","required");
 		$valid->set_rules("password","Password","required");
 		if($valid->run()) {
-		$this->simple_login->login($username,$password, base_url("dasbor"), base_url("login"));
+		$this->simple_login->login($username,$password, base_url("dasbor"), base_url("Auth/login"));
 		}
 		// End fungsi login
 
 		$data = array('title'=>'Halaman Login Admin');
 		$this->load->view('admin/login_view',$data);
-		
-		
 	}
 
 	//Fungsi logout

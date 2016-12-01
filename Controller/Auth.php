83f71e7c93875487b18d54319aeade770c227497
Burhanuddin Ahmad <burhanahmeed@gmail.com>
2016-12-01 15:51:47 +0700

    Update Auth.php

--- Controller/Auth.php
+++ Controller/Auth.php
@@ -7,7 +7,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @extends CI_Controller
  */
 class Auth extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
 
+		$this->load->library('Bcrypt');
+		$this->load->model('User_model');
+		$this->load->database();
+	}
 	
 	public function index() {
 		redurect('Auth/login');		
@@ -23,11 +30,25 @@ class Auth extends CI_Controller {
 		//fungsi login
 		$valid = $this->form_validation;
 		$username = $this->input->post("username");
-		$password = md5($this->input->post("password"));
+		$password = $this->input->post("password");
 		$valid->set_rules("username","Username","required");
 		$valid->set_rules("password","Password","required");
-		if($valid->run()) {
-		$this->simple_login->login($username,$password, base_url("dasbor"), base_url("Auth/login"));
+		
+		if ($result = $this->User_model->getLogin($username)) {
+			foreach ($result as $row) {
+				$stored_hash = $row->password;
+				if ($this->bcrypt->check_password($password,$stored_hash)) {
+					if($valid->run()) {
+					 $this->simple_login->login($username,$stored_hash, base_url("dasbor"), base_url("Auth/login"));
+					} 
+
+				} else{
+					$this->session->set_flashdata('sukses','Oops.. Username/password salah');
+			 		redirect(base_url('Auth/login'));
+				}
+			}
+		} else{
+
 		}
 		// End fungsi login
 

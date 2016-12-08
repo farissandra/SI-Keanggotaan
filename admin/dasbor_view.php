6722c36b6c1ae9b4bd39d04a0edf3423d423bb02
DewiAprilia08 <dewiaprilia720@yahoo.com>
2016-12-08 08:46:00 +0700

    Create dasbor_view.php

---
+++ admin/dasbor_view.php
@@ -0,0 +1,29 @@
+
+<div class="dasbor_body">
+	<div class="container" style="margin-bottom: 10px">
+
+<div class="col-md-6">
+	<div style="">
+		<h4>Jumlah Anggota Terdaftar</h4>
+	</div>
+		<h5>
+		<?php 
+		$this->db->select('*');
+		$this->db->from('anggota');
+		echo $this->db->count_all_results();
+		?>
+		</h5>
+</div>
+
+				<!--Navigasi	-->
+<div class="col-md-6" style="text-align: center; float: right;">
+	<div class="button-kopma" style="color: white">
+		<a href="<?php echo site_url('anggota1');?>">Daftar Data Anggota</a>
+	</div>
+	<div class="button-kopma" style="color: white">
+		<a href="">Data Iuran Anggota</a>
+	</div>
+</div>
+
+	</div>
+</div>

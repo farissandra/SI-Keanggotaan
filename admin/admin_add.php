bdff1cb340735265be0016067a7555fc496130a9
DewiAprilia08 <dewiaprilia720@yahoo.com>
2016-12-08 08:42:46 +0700

    Create admin_add.php

---
+++ admin/admin_add.php
@@ -0,0 +1,33 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 well">
+			<?php $attributes = array("name" => "signupform");
+			echo form_open("User/tambah", $attributes);?>
+			<legend>Tambah Admin</legend>
+			
+			<div class="form-group">
+				<label for="name">Username</label>
+				<input class="form-control" name="username" placeholder="Username" type="text" value="<?php echo set_value('username'); ?>" />
+				<span class="text-danger"><?php echo form_error('username'); ?></span>
+			</div>	
+
+			<div class="form-group">
+				<label for="subject">Password</label>
+				<input class="form-control" name="password" placeholder="Password" type="password" />
+				<span class="text-danger"><?php echo form_error('password'); ?></span>
+			</div>
+
+			<div class="form-group">
+				<label for="subject">Confirm Password</label>
+				<input class="form-control" name="cpassword" placeholder="Confirm Password" type="password" />
+				<span class="text-danger"><?php echo form_error('cpassword'); ?></span>
+			</div>
+
+			<div class="form-group">
+				<button name="submit" type="submit" class="btn btn-info">Signup</button>
+				<button name="cancel" type="reset" class="btn btn-info">Cancel</button>
+			</div>
+			<?php echo form_close(); ?>
+			<?php echo $this->session->flashdata('msg'); ?>
+		</div>
+	</div>

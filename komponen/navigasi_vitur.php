4a2b998588083f3174c604439aa4f2a85ea0c97c
DewiAprilia08 <dewiaprilia720@yahoo.com>
2016-12-08 08:39:03 +0700

    Create navigasi_vitur.php

---
+++ komponen/navigasi_vitur.php
@@ -0,0 +1,10 @@
+<div class="container" style="margin-bottom: 10px">
+<div class="col-md-12" style="text-align: center;">
+	<div class="button-kopma" style="color: white">
+		<a href="<?php echo site_url('anggota1');?>">Daftar Data Anggota</a>
+	</div>
+	<div class="button-kopma" style="color: white">
+		<a href="<?php echo site_url('iuran');?>">Data Iuran Anggota</a>
+	</div>
+	</div>
+</div>

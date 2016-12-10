8dd886663f409a376d2344581d4df028209198c0
Burhanuddin Ahmad <burhanahmeed@gmail.com>
2016-12-10 19:47:34 +0700

    Update header_general.php

--- view/header_general.php
+++ view/header_general.php
@@ -1,40 +1,4 @@
-<!-- Start Header -->
-	
-    <!-- Start Nav -->
 
-    <!--<nav class="navbar navbar-all">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#important-id-for-collapsing" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar" style="background-color:white"></span>
-                <span class="icon-bar" style="background-color:white"></span>
-                <span class="icon-bar" style="background-color:white"></span>
-            </button>
-            <div class="logo-header">
-                <li style="list-style-type: none; background-color: #e6e6e6; margin: 0px; padding: 5px;"><img style="height: 40px;" src="<?php echo base_url('');?>assets/images/kopma-logo.png"/></li>
-            </div>
-            <li style="padding: 14px 16px; color: white; list-style-type: none;">Sistem Informasi Anggota KOPMA dr.ANGKA ITS</li>
-        </div>
-        
-<!===========================Float left==================================-->      
-<!--<div class="nav-right">     
-<!===========================Float right==================================-->
-    <!--<div class="collapse navbar-collapse" id="important-id-for-collapsing">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">DATA ANGGOTA</a></li>
-                    <li><a href="#">DATA IURAN</a></li>
-                </ul>
-            </div>
-        </div>
-
-    </div>
-    </nav>
-
-
-
-    <div class="container">-->
 
 <body>
 	<nav class="navbar navbar-inverse">
@@ -46,9 +10,26 @@
 	      <li><a href="<?php echo base_url();?>">Home</a></li>
 	      <li><a href="<?php echo base_url();?>AnggotaData">Data Anggota</a></li>
 	      <li><a href="#">Data Iuran</a></li>
+		     <li><a href="#" id="myBtn">APA INI</a></li>
 	    </ul>
         <div class="left-nav">
             <img src="<?php echo base_url();?>assets/images/kopma-logo.png">
         </div>
 	  </div>
 	</nav>
+	
+		<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>SIM Anggota Merupakan Sistem Informasi Anggota KOPMA dr.Angka ITS. Dengan SIM ini diharapkan anggota dapat lebih berpartisipasi aktif dalam KOPMA dr.Angka ITS.</p></br>
+    <h4>Apa Yang Bisa Kami Lakukan?</h4><br>
+    <p>1. Dengan adanya SIM ini kalian dapat melakukan pengecekan terhadap data diri kalian.</p>
+    <p>2. Melakukan pengecekan terhadap status iuran kalian sehingga dapat membantu mengetahui kelunasan iuran bulanan kalian.</p>
+  </div>
+
+</div>
+
+<script type="text/javascript" src="<?php echo base_url(''); ?>assets/js/jquery.js"></script>

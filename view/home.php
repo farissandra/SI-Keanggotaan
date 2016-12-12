9d7657712355d3f927884550ab2e5fb323439d58
bennytimotius <bennybtc03@gmail.com>
2016-12-12 20:18:07 +0700

    Update home.php

--- view/home.php
+++ view/home.php
@@ -27,9 +27,28 @@
 		</div>
 		<!--Sisi Kanan -->
 		<div class="col-md-6">
-			<h2>Sekretariat :</h2><br>
-			<h4>Gedung SCC lt.1 Kampus ITS Sukolilo</h4>
+			<div class="moneymaker-judul">
+			<h3>More Information</h3><br>
+			</div>
+
+			<div class="socmed"><a href="https://www.facebook.com/Kopma-dr-Angka-ITS-335534323180404/"><img src="<?php echo base_url()?>assets/images/fb.png" style="height: 30px"></a></div>
+
+			<div class="socmed"><a href="https://www.instagram.com/kopma_its/"><img src="<?php echo base_url()?>assets/images/ig.png" style="height: 30px"></a></div>
+
+			<div class="socmed"><a href="https://twitter.com/kopma_its"><img src="<?php echo base_url()?>assets/images/twitter.png" style="height: 30px"></a></div>
+
+			<div class="socmed"><a href="http://line.me"><img src="<?php echo base_url()?>assets/images/line.png" style="height: 30px"></a></div>
+		
+		<div class="moneymaker-kontak" style="text-align: left">
+			<h6><img src="<?php echo base_url()?>assets/images/loc.png" style="height: 20px; padding-right:  15px">Gedung SCC lt.1 Kampus ITS Sukolilo</h6>
+			<h6><img src="<?php echo base_url()?>assets/images/tel.png" style="height: 20px; padding-right: 15px">0857 2312 1231</h6>
 		</div>
+		
+		</div>
+
+		</div>
+	</div>
+
 
 		</div>
 	</div>
